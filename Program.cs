a0403ad801b49c30a03ded27a597a60df9335f08
IGOR GUTIERREZ <ac.igor.gutierrez.dc@gmail.com>
2023-07-24 15:10:21 -0300

    First Commit.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace GoProxyTool
+{
+    static class Program
+    {
+        //[STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new ProxySetingWin());
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# dotNetCore-GoProxyTool-Acigo
- Proxy in C# dotNet Core
+# gptool-csharp
+snail007/goproxy C#开发的Windows桌面版配置工具，由于个人能力有限，欢迎有志之士贡献代码一起完善`gptool-csharp`
+
+### 界面
+
+![snapshot.png](/doc/images/snapshot0.png)
